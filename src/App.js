1d34bd21c660cf02ecaf4891b540b2f8d1cb05e2
yahoo100k <rajapple876@gmail.com>
2023-02-28 12:31:41 -0500

    added portfolio

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import React from 'react';
-import './App.css';
-import Button from './components/button'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faTiktok, faYoutube, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import "./App.css";
+import Button from "./components/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+    faGithub,
+  faTiktok,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+const socialHandles = [
+  {
+    name: "instagram",
+    link: "https://www.instagram.com/rajae876__",
+    icon: faInstagram,
+  },
+  {
+    name: "Tiktok",
+    link: "https://www.tiktok.com/@rajae876__?_t=8Uo4PZvcxFu&_r=1",
+    icon: faTiktok,
+  },
+  {
+    name: "Twitter",
+    link: "https://www.twitter.com/rajae876_",
+    icon: faTwitter,
+  },
+  {
+    name: "Portfolio",
+    link: "https://rajaedev.netlify.app",
+    icon: faGithub,
+  },
+];
 
 const App = () => {
-    const instagramLink = "https://www.instagram.com/rajae876__"
-    const youtubeLink = "https://www.youtube.com"
-    const tiktokLink = "https://www.tiktok.com/@rajae876__?_t=8Uo4PZvcxFu&_r=1"
-    const twitterLink = "https://www.twitter.com"
-
-    return (
+  return (
     <div className="content">
       <div className="container">
-      <span className="profile_photo"></span>
+        <span className="profile_photo"></span>
         <span className="profile_name">Rajae socials😎</span>
-        <Button name={"Instagram"} icon={<FontAwesomeIcon icon={faInstagram} size="lg" className="fa fa-instagram"/>} link={instagramLink} />
-        <Button name={"Tiktok"} icon={<FontAwesomeIcon icon={faTiktok} size="lg" className="fa fa-tiktok"/>} link={tiktokLink}/>
-        <Button name={"Twitter"} icon={<FontAwesomeIcon icon={faTwitter} size="lg" className="fa fa-twitter"/>} link={twitterLink}/>
-        <Button name={"Youtube"} icon={<FontAwesomeIcon icon={faYoutube} size="lg" className="fa fa-youtube"/>} link={youtubeLink}/>
+                <FontAwesomeIcon icon="fas fa-user" />
+        {socialHandles.map((item) => (
+          <Button
+            name={item.name}
+            icon={
+              <FontAwesomeIcon
+                icon={item.icon}
+                size="lg"
+                className={`fa fa-${item.name}`}
+              />
+            }
+            link={item.link}
+          />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './button.css';
 
 export default function Button({link, icon, name}){
-    const [hover, setHover] = useState(false)
+    const [hover, setHover] = useState(false);
     return (
         <a className = {hover ? 'buttonhover' : ''} href={link}
         onMouseEnter = {() => setHover(true)}
